2186fac2fe195d9418a16fa08c42ea1b66fdaf8f
joaqu1m <joaqu1m.pires@hotmail.com>
2025-05-13 03:45:31 +0000

    meta verify whatsapp test

--- functions/message-standardizer/main.go
+++ functions/message-standardizer/main.go
@@ -9,9 +9,28 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+const verifyToken = "your_verify_token"
+
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	requestJSON, _ := json.MarshalIndent(request, "", "  ")
 	log.Printf("Received request: %s", requestJSON)
+
+	if request.HTTPMethod == "GET" {
+		mode := request.QueryStringParameters["hub.mode"]
+		token := request.QueryStringParameters["hub.verify_token"]
+		challenge := request.QueryStringParameters["hub.challenge"]
+
+		if mode == "subscribe" && token == verifyToken {
+			log.Println("Webhook verificado com sucesso")
+			return events.APIGatewayProxyResponse{
+				StatusCode: 200,
+				Body:       challenge,
+			}, nil
+		}
+
+		log.Println("Token de verificação inválido")
+		return events.APIGatewayProxyResponse{StatusCode: 403}, nil
+	}
 
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
