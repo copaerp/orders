475ff0bb1ced902fc1d5ddb71a1c629fac264b54
joaqu1m <joaqu1m.pires@hotmail.com>
2025-06-01 02:58:03 +0000

    understanding gorm joins

--- shared/repositories/order.go
+++ shared/repositories/order.go
@@ -17,7 +17,7 @@ func (c *OrdersRDSClient) GetActiveOrderByCustomerAndSender(customerID, senderID
 		Joins("Unit").
 		Joins("Unit.WhatsappNumber").
 		Where("Customer.id = ?", customerID.String()).
-		Where("Unit.WhatsappNumber.id = ?", senderID.String()).
+		Where("Unit__WhatsappNumber.id = ?", senderID.String()).
 		Where("Order.finished_at IS NULL").
 		First(&order).Error
 
@@ -36,7 +36,7 @@ func (c *OrdersRDSClient) GetActiveOrderByCustomerAndSenderNumbers(customerNumbe
 		Joins("Unit").
 		Joins("Unit.WhatsappNumber").
 		Where("Customer.phone = ?", customerNumber).
-		Where("Unit.WhatsappNumber.number = ?", senderNumber).
+		Where("Unit__WhatsappNumber.number = ?", senderNumber).
 		Where("Order.finished_at IS NULL").
 		First(&orders).Error
 
