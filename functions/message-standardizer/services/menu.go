29ff2923340384e580a62d30958286c7bb228d2b
joaqu1m <joaqu1m.pires@hotmail.com>
2025-06-08 17:33:41 +0000

    adding item id to menu

--- functions/message-standardizer/services/menu.go
+++ functions/message-standardizer/services/menu.go
@@ -7,21 +7,21 @@ import (
 	"github.com/google/uuid"
 )
 
-func MountMenu(rdsClient *repositories.OrdersRDSClient, unitID uuid.UUID) (menu map[string]map[string]string, err error) {
+func MountMenu(rdsClient *repositories.OrdersRDSClient, unitID uuid.UUID) (menu []map[string]string, err error) {
 
 	products, err := rdsClient.GetProductsByUnitID(unitID)
 	if err != nil {
 		return nil, err
 	}
 
-	menu = make(map[string]map[string]string, len(products))
-
+	menu = make([]map[string]string, len(products))
 	for i, product := range products {
-		menu[product.ID.String()] = map[string]string{
-			"index":       strconv.Itoa(i + 1),
+		menu[i] = map[string]string{
+			"id":          product.ID.String(),
+			"index":       strconv.Itoa(i + 1), // Não estamos mais utilizando o índice devido aos testes com IA
 			"name":        product.Name,
 			"description": product.Description,
-			"price":       product.BRLPrice.StringFixed(4),
+			"price":       product.BRLPrice.StringFixed(2),
 			"category":    product.Category,
 		}
 	}
